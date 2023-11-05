0fb3851611869622cdf7878181d61e8b6515c5c5
dimitrije-r <dimitrije.rankov@outlook.com>
2023-11-05 23:56:54 +0100

    First commit

---
+++ 2-notes/src/NoteList.tsx
@@ -0,0 +1,20 @@
+import { Button, Col, Row, Stack } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+const NoteList = () => {
+  return (
+    <>
+        <Row>
+            <Col><h1>Notes</h1></Col>
+            <Col xs="auto">
+                <Stack gap={2} direction="horizontal">
+                    <Link to="/new">
+                        <Button variant="primary">Create</Button>
+                    </Link>
+                </Stack>
+            </Col>
+        </Row>
+    </>
+  )
+}
+export default NoteList
