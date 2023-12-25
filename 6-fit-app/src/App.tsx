0d4b5aee32cfae83d0e29cac23e01832c630795d
dimitrije-r <dimitrije.rankov@outlook.com>
2023-12-25 02:49:28 +0100

    gym update

---
+++ 6-fit-app/src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import OurClasses from "./components/ourClasses";
+import Benefits from "./components/benefits";
+import ContactUs from "./components/contactUs";
+import Footer from "./components/footer";
+import { SelectedPage } from "./shared/types";
+
+const App=() =>{
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-<h2>React + TypeScript</h2><br>
+<div align="center"><h2>React + TypeScript</h2><br> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> <img src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg" alt="typescript" width="40" height="40"/> <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwindcss" width="40" height="40"/> <img src="https://upload.vectorlogo.zone/logos/getbootstrap/images/987f8f6c-263a-47b1-a85d-853cfca215d9.svg" alt="bootstrap" width="40" height="40"/></div><br>
 
 https://1-hangman.netlify.app/
 <br>https://2-notes.netlify.app/
+<br>https://3-shopping-cart.netlify.app/
 <br>https://4-quiz.netlify.app/
 <br>https://5-yt-homepage-clone.netlify.app/
-<br>
 <hr>
 Packages used for <b>2-notes</b>:<hr>
  react-router-dom
@@ -22,3 +22,7 @@ Packages used for <b>2-notes</b>:<hr>
 <hr>Packages used for <b>4-quiz</b>:<hr>
 
 styled-components (with <i><b>@types/styled-components</b></i> to get the types)
+
+<hr>Packages used for <b>5-youtube-copy</b>:<hr>
+
+tailwindcss
