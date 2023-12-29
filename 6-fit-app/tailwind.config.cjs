0ea61febf2d4590a1ff2995a6468d965b41e80e3
dimitrije-r <dimitrije.rankov@outlook.com>
2023-12-29 16:56:33 +0100

    update2

--- 6-fit-app/tailwind.config.cjs
+++ 6-fit-app/tailwind.config.cjs
@@ -12,29 +12,29 @@ module.exports = {
         "primary-300": "#FFA6A3",
         "primary-500": "#FF6B66",
         "secondary-400": "#FFCD5B",
-        "secondary-500": "#FFC132",
+        "secondary-500": "#FFC132"
       },
       backgroundImage: (theme) => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-        "mobile-home": "url('./assets/HomePageGraphic.png')",
+        "mobile-home": "url('./assets/HomePageGraphic.png')"
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
-        montserrat: ["Montserrat", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"]
       },
       content: {
         evolvetext: "url('./assets/EvolveText.png')",
         abstractwaves: "url('./assets/AbstractWaves.png')",
-        sparkles: "url('./assets/Sparkles.png')",
-        circles: "url('./assets/Circles.png')",
-      },
+        sparkles: "url('./assets/sparkles.png')",
+        circles: "url('./assets/Circles.png')"
+      }
     },
     screens: {
       xs: "480px",
       sm: "768px",
-      md: "1060px",
+      md: "1060px"
     },
   },
-  plugins: [],
+  plugins: []
 };
